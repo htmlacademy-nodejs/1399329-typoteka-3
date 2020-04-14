0bb3cbfda85169381c174ef31c1274704c3f353d
behemothoz <behemothoz@yandex.ru>
2020-04-14 18:56:50 +0300

    add new cli command - 'server' + run simple server

--- src/constants/index.js
+++ src/constants/index.js
@@ -4,12 +4,22 @@ const COMMANDS = {
   HELP: `--help`,
   VERSION: `--version`,
   GENERATE: `--generate`,
+  SERVER: `--server`
+};
+
+const HttpCode = {
+  OK: 200,
+  NOT_FOUND: 404,
+  INTERNAL_SERVER_ERROR: 500,
+  FORBIDDEN: 403,
+  UNAUTHORIZED: 401,
 };
 
 module.exports = {
   COMMANDS,
   DEFAULT_COMMAND: COMMANDS.HELP,
   USER_ARGV_INDEX: 2,
+  HttpCode,
   ExitCode: {
     error: 1,
     success: 0,
--- src/service/cli/help.js
+++ src/service/cli/help.js
@@ -1,14 +1,15 @@
 'use strict';
 
 const log = require(`./console`);
-const {HELP, VERSION, GENERATE} = require(`../../constants`).COMMANDS;
+const {HELP, VERSION, GENERATE, SERVER} = require(`../../constants`).COMMANDS;
 
 const helperText = `
-  Программа формирует файл с данными (mock) для API.
+  Программа запускает http-сервер и формирует файл с данными (mock) для API.
   Команды:
   ${HELP}:               печатает этот текст
   ${VERSION}:            выводит номер версии
   ${GENERATE} <count>    формирует файл mocks.json
+  ${SERVER} <port>       запускает http-сервер
 `;
 
 module.exports = {
