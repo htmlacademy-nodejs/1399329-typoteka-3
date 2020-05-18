6ac1541903c11b6bbb15f1b1dc362a6470ff235f
behemothoz <behemothoz@yandex.ru>
2020-05-18 21:57:36 +0300

    fix eslint

--- src/service/handlers/errorHandler.js
+++ src/service/handlers/errorHandler.js
@@ -7,6 +7,6 @@ const INTERNAL_SERVER_MSG_TEXT = `Something went wrong`;
 // eslint-disable-next-line  no-unused-vars
 const errorHandler = (err, req, res, next) => {
   res.status(HttpCode.INTERNAL_SERVER_ERROR).send(INTERNAL_SERVER_MSG_TEXT);
-}
+};
 
 module.exports = errorHandler;
--- src/service/handlers/notFoundRouteHandler.js
+++ src/service/handlers/notFoundRouteHandler.js
@@ -6,6 +6,6 @@ const NOT_FOUND_MSG_TEXT = `Not found`;
 
 const notFoundRouteHandler = (_, res) => {
   res.status(HttpCode.NOT_FOUND).send(NOT_FOUND_MSG_TEXT);
-}
+};
 
 module.exports = notFoundRouteHandler;
