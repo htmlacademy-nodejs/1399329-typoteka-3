980f37f5745a892d9cca725d151e5e59535acebb
behemothoz <behemothoz@yandex.ru>
2020-07-26 15:23:34 +0300

    add id for publication / add field - comments

--- src/constants/generate.js
+++ src/constants/generate.js
@@ -4,6 +4,8 @@ module.exports = {
   DEFAULT_COUNT: 1,
   MAX_PUBLICATIONS: 1000,
   MAX_ANNOUNCE_COUNT: 5,
+  MAX_ID_LENGTH: 6,
+  MAX_COMMENTS: 4,
   MONTH_COUNT: 3,
   FILE_NAME: `mocks.json`
 };
